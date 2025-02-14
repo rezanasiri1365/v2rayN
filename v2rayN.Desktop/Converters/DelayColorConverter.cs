832a167978b5a815567280f41cdd056e6e19bf91
reza <reza@RN01>
2025-02-14 18:21:00 +0330

    Add project files.

---
+++ v2rayN.Desktop/Converters/DelayColorConverter.cs
@@ -0,0 +1,26 @@
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace v2rayN.Desktop.Converters
+{
+    public class DelayColorConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            int.TryParse(value?.ToString(), out var delay);
+
+            if (delay <= 0)
+                return new SolidColorBrush(Colors.Red);
+            if (delay <= 500)
+                return new SolidColorBrush(Colors.Green);
+            else
+                return new SolidColorBrush(Colors.IndianRed);
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
